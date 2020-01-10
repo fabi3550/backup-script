d6a1fb5248165039d1b2b92a794d1d0927b6a6f0
Fabian Taubald <fabian@fabiantaubald.de>
2020-01-10 19:59:51 +0100

    Dateien angelegt

---
+++ backup.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+logmessage() {
+    echo $(date +"%H:%M:%S"): $1
+}
+
+# read input parameters
+while [ "$1" != '' ]
+    do
+        [ "$1" == --scp ] && scp_path="$2" && shift && shift
+        [ "$1" == --file ] && target_filename="$2" && shift && shift
+        [ "$1" == --pgp ] && pgp_key="$2" && shift && shift
+    done
+
+# define backup filename
+backup_filename=$(date +"%Y_%m_%d").tar.gz
+
+#create a tar file by parameterfile
+logmessage "create a tar file"
+tar -cjf $backup_filename -T backup.conf
+
+#encrypt with defined pgp key
+if [ -n "$pgp_key" ]
+then
+    logmessage "encrypt the tar file"
+    gpg2 --encrypt --recipient $pgp_key $backup_filename
+    logmessage "deleting original tar file"
+    rm $backup_filename
+    backup_filename=$backup_filename.gpg
+fi
+
+if [ -n "$scp_path" ]
+then
+    logmessage "transfer backup via scp to $scp_path"
+    scp $backup_filename $scp_path
+fi
+
+if [ -n "$target_filename" ]
+then
+    logmessage "copy backup to $target_filename"
+    scp $backup_filename $target_filename
+fi
+
+logmessage "delete temporary backup file"
+rm $backup_filename
